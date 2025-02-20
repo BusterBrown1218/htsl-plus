f711b6aea5d2cadd1927abcbf45e1fb884472cbb
Sin_ender <ender@sinender.net>
2025-02-19 22:03:20 -0500

    updated to be the same as ptsl

--- extension.js
+++ extension.js
@@ -20,9 +20,20 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ptsl-plus.startsocket', function () {
+	const disposable = vscode.commands.registerCommand('ptsl-plus.startsocket', async function () {
+		const userResponse = await vscode.window.showInputBox({
+			placeHolder: 'Type the port number to start the websocket server',
+			value: '7000'
+		  });
+		if (!userResponse) 
+			return;
+		const port = parseInt(userResponse);
+		if (isNaN(port)) {
+			vscode.window.showErrorMessage('Invalid port number');
+			return
+		}
 		// The code you place here will be executed every time your command is executed
-		ws = new WebSocketServer({ port: 7000 });
+		ws = new WebSocketServer({ port: port });
 		ws.on("connection", (socket) => {
 			socket.on("message", (message) => {
 				console.log("Received: " + message);
@@ -30,7 +41,7 @@ function activate(context) {
 			});
 		});
 		// Display a message box to the user
-		vscode.window.showInformationMessage('PTSL+ Websocket Server started on port 7000');
+		vscode.window.showInformationMessage('PTSL+ Websocket Server started on port ' + port);
 	});
 
 
